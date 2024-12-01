6e4a6e7c38f33024f092fc29407e9e0289e96472
Iker <velezlucasiker1@gmail.com>
2024-12-01 16:00:07 -0500

    feat: Styled main.ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 //Routes
 router.get('', (req, res) => {
-    res.render('index');
+   const locals = {
+        title: 'Home Page',
+        description: 'This is the home page of the 42 Blog',
+   } 
+
+    res.render('index', { locals, showSidebar: true });
 });
+
+//This is true if login and registration form are false
+let showSidebar = true;
+//
 
 module.exports = router;
