74646fcf61c82684973763d43259c531cce91ae9
Iker <velezlucasiker1@gmail.com>
2024-12-04 23:33:24 -0500

    feat: Create posts route and view

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,19 +6,64 @@ const Post = require('../models/Post');
 
 // Get Home//
 router.get('', async (req, res) => {
-   const locals = {
-        title: 'Home Page',
-        description: 'This is the home page of the 42 Blog',
-   } 
-    
    try {
-     const data = await Post.find();
-     res.render('index', { locals, data, showSidebar: true });
+     const locals = {
+          title: 'Home Page',
+          description: 'This is the home page of the 42 Blog',
+     }
+     
+     let perPage = 10;
+     let page = req.query.page || 1;
+
+     const data = await Post.aggregate([{ $sort: { createdAt: -1 }}])
+     .skip(perPage * page - perPage)
+     .limit(perPage)
+     .exec();
+     
+     const count = await Post.countDocuments();
+     const nextPage = parseInt(page) + 1;
+     const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+     res.render('index', { 
+          locals,
+          data,
+          current: page,
+          nextPage: hasNextPage ? nextPage : null,
+          showSidebar: true });
    } catch (error) {
      console.log(error);
    }
 
 });
+
+//Post Search//
+
+router.post('/search', async (req, res) => {
+     try {
+          const locals = {
+               title: 'Search',
+               description: 'This is the about page of the 42 Blog',
+           }
+
+           let searchTearm = req.body.searchTerm;
+           const searchNoSpecialChar = searchTearm.replace(/[^a-zA-Z0-9]/g, '');
+
+          const data = await Post.find({
+               $or: [
+                    { title: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+                    { body: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+               ]
+          });
+
+          res.render("search", {
+               data,
+               locals,
+               showSidebar: true
+          });
+     } catch (error) {
+          console.log(error);
+     }
+ });
 
 //Get About//
 router.get('/about', (req, res) => {
@@ -51,5 +96,24 @@ let showSidebar = true;
      ])
 }
 insertPostData(); */
+
+//Get Post//
+router.get('/post/:id', async (req, res) => {
+     try {
+          
+          let slug = req.params.id;
+
+          const data = await Post.findById({ _id: slug });
+
+          const locals = {
+               title: data.title,
+               description: 'This is the post page of the 42 Blog',
+          }
+
+          res.render('post', { locals, data, showSidebar: true });
+     } catch (error) {
+          console.log(error);
+     }
+});
 
 module.exports = router;
